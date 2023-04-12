c5d54f674ca849d75f92af4970b9ecafb3e9e2ab
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2023-04-12 14:57:59 -0600

    Make linter happy

--- tap_messagebird/streams.py
+++ tap_messagebird/streams.py
@@ -43,10 +43,10 @@ class ConversationsStream(MessagebirdConversations):
     # Optionally, you may also use `schema_filepath` in place of `schema`:
     schema_filepath = SCHEMAS_DIR / "conversation.json"
 
-    def get_child_context(self, record: dict, context: Optional[dict]) -> dict | None:
+    def get_child_context(self, record: dict, context: Optional[dict]) -> Optional[dict]:
         """Return a context dictionary for child streams."""
         if record["status"] == "archived":
-            return
+            return None
 
         return {
             "_sdc_conversations_id": record["id"],
