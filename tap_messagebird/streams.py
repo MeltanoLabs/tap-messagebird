7d5b3c8adeff9df2e47b1013399d27cdd1665e53
Derek Visch <dvisch@autoidm.com>
2023-04-25 13:57:32 -0400

    Tried to address linting issues

--- tap_messagebird/streams.py
+++ tap_messagebird/streams.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 from pathlib import Path
-from typing import Any
+from typing import Any, Iterable
 
 from tap_messagebird.client import MessagebirdOffsetPaginator, MessagebirdStream
 
@@ -95,7 +95,7 @@ class ConversationMessagesStream(MessagebirdConversations):
             params["from"] = self.config["start_date"]
         return params
 
-    def get_records(self, context: dict | None):
+    def get_records(self, context: dict | None) -> Iterable[dict[str, Any]]:
         """Return a generator of record-type dictionary objects.
 
         Each record emitted should be a dictionary of property names to their values.
@@ -111,14 +111,18 @@ class ConversationMessagesStream(MessagebirdConversations):
         except ConversationArchivedFailure as e:
             self.logger.warning(f"Conversation is archived, skipping. {e=}")
 
-    def validate_response(self, response):
+    def validate_response(self, response) -> None:
+        """If a conversation is archived before we pull the message we sometimes get a 410.
+
+        We don't want to fail the sync for this, so we catch it and log it.
+        """
         if response.status_code == 410:
             msg = (
                 f"{response.status_code} Client Error: "
                 f"{response.reason} for url: {response.url}"
             )
-            response_json = response.json()
-            error: dict = response_json.get("error")
+            response_json: dict = response.json()
+            error: dict = response_json["error"]
             if response.status_code == 410 and error[0]["code"] == 21:
                 raise ConversationArchivedFailure(msg + f" {error=}")
         super().validate_response(response)
@@ -149,5 +153,5 @@ class MessagesStream(MessagebirdStream):
         return params
 
 
-class ConversationArchivedFailure(Exception):
+class ConversationArchivedWarning(Exception):
     """Conversation is archived and it threw in error."""
